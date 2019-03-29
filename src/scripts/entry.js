f99865d3d4336c712993fc1fb3752351e3981602
shelooks16 <blauyedz@zoho.eu>
2019-03-29 17:39:37 +0300

    pwa; foit gone

--- src/scripts/entry.js
+++ src/scripts/entry.js
@@ -2,4 +2,11 @@ import 'normalize.css';
 import '../styles/all.scss';
 import loadBuddies from './loadBuddies';
 
+if ('serviceWorker' in navigator) {
+  const swName = "/service-worker.js";
+  navigator.serviceWorker.register(swName).catch(function(ex) {
+    console.warn(ex);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", loadBuddies);
