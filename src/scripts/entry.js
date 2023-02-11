dfa62387601cc2579d21b7fedee47922ba5ef7a7
shelooks16 <blauyedz@zoho.eu>
2023-02-11 18:57:11 +0000

    disble sw locally

--- src/scripts/entry.js
+++ src/scripts/entry.js
@@ -2,7 +2,10 @@ import 'normalize.css';
 import '../styles/all.scss';
 import loadBuddies from './loadBuddies';
 
-if ('serviceWorker' in navigator) {
+const isLocalHost =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1";
+
+if ('serviceWorker' in navigator && !isLocalHost) {
   const swName = "/service-worker.js";
   navigator.serviceWorker.register(swName).catch(function(ex) {
     console.warn(ex);
